23a5e52d13d13f7c3fca189908eb46fcfd1380cf
JingjingJi94 <Jingjingji94@gmail.com>
2025-03-15 18:21:37 -0700

    Update candidate_response.py

--- backend/app/db/candidate_response.py
+++ backend/app/db/candidate_response.py
@@ -7,10 +7,10 @@ candidate_response = {
         3: "Diplomatic",
         4: "Brainstorm ideas",
         5: "Idea generator",
-        6: "Somewhat",
-        7: "Frequently",
-        8: "Team spirit",
-        9: "By impact",
-        10: "Ask for help"
+        # 6: "Somewhat",
+        # 7: "Frequently",
+        # 8: "Team spirit",
+        # 9: "By impact",
+        # 10: "Ask for help"
     }
 }
